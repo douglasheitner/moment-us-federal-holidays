a1a58455d3ed5a1baa01185a589f72f3b19eac7d
Douglas Heitner <douglas.heitner@gmail.com>
2017-01-29 11:30:32 -0600

    Initial Commit

---
+++ moment-us-federal-holidays.js
@@ -0,0 +1,84 @@
+//Days:   Sunday-Saturday (0-6)
+//Months: January-December (0-11)
+//
+
+var currentYear = moment().year();
+
+// New Year's Day - January 1
+var newYears = moment('1/1/'+currentYear).day();
+switch (newYears) {
+  case 0: //Sunday
+    newYears = moment('1/1/'+currentYear).day(1).format('L');
+    break;
+  case 6: //Saturday
+    newYears = moment('1/1/'+currentYear).day(5).format('L');
+    break;
+  default:
+    newYears = moment('1/1/'+currentYear);
+}
+
+// Martin Luther King, Jr. Day - Third Monday in January
+var martinLutherKingJrDay = moment('1/1/'+currentYear).day(1);
+switch (martinLutherKingJrDay.month()) {
+  case 11:
+    martinLutherKingJrDay = moment('1/1/'+currentYear).day(22).format('L');
+    break;
+  default:
+    martinLutherKingJrDay = moment('1/1/'+currentYear).day(15).format('L');
+}
+
+
+// George Washington’s Birthday - Third Monday in February
+var washingtonBirthday = moment('2/1/'+currentYear).day(1);
+switch (washingtonBirthday.month()) {
+  case 0:
+    washingtonBirthday = moment('2/1/'+currentYear).day(22).format('L');
+    break;
+  default:
+    washingtonBirthday = moment('2/1/'+currentYear).day(15).format('L');
+}
+
+// Memorial Day - Last Monday in May
+var memorialDay = moment('5/31/'+currentYear).day(1).format('L');
+
+// Independence Day - July 4
+var indepenceDay = moment('7/4/'+currentYear).day();
+switch (indepenceDay) {
+  case 0: //Sunday
+    indepenceDay = moment('7/4/'+currentYear).day(1).format('L');
+    break;
+  case 6: //Saturday
+    indepenceDay = moment('7/4/'+currentYear).day(5).format('L');
+    break;
+  default:
+    indepenceDay = moment('7/4/'+currentYear);
+}
+
+// Labor Day - First Monday in September
+var laborDay = moment('9/1/'+currentYear).day(1);
+switch (laborDay.month()) {
+  case 7: //August
+    laborDay = moment('9/1/'+currentYear).day(8).format('L');
+    break;
+  default:
+    laborDay = moment('9/1/'+currentYear).day(1).format('L');
+}
+
+// Columbus Day - Second Monday in October
+var columbusDay = moment('10/1/'+currentYear).day(1);
+switch (columbusDay.month()) {
+  case 8: //September
+    columbusDay = moment('10/1/'+currentYear).day(15).format('L');
+    break;
+  default:
+    columbusDay = moment('10/1/'+currentYear).day(8).format('L');
+}
+
+// Veterans Day - November 11
+var veteransDay = moment('11/11/'+currentYear).format('L');
+
+// Thanksgiving Day - 4th Thursday in November
+var thanksgivingDay = moment('11/1/'+currentYear).day(29).format('L')
+
+// Christmas Day - December 25
+var christmasDay = moment('12/25/'+currentYear).format('L');
