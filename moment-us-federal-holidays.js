f12395b1514bc016ac87c8e6eef4e5c51ccdd16b
Douglas Heitner <dheitner@exploregci.com>
2017-01-30 13:53:57 -0600

    Implemented isNationalHoliday function.

--- moment-us-federal-holidays.js
+++ moment-us-federal-holidays.js
@@ -1,117 +1,134 @@
 //Days:   Sunday-Saturday (0-6)
 //Months: January-December (0-11)
 
-var currentYear = moment().year();
+var year = moment().year();
 
 // New Year's Day - January 1
-var newYears = moment('1/1/'+currentYear).day();
+var newYears = moment('1/1/'+year).day();
 switch (newYears) {
   case 0: //Sunday
-    newYears = moment('1/1/'+currentYear).day(1).format('L');
+    newYears = moment('1/1/'+year).day(1).format('L');
     break;
   case 6: //Saturday
-    newYears = moment('1/1/'+currentYear).day(5).format('L');
+    newYears = moment('1/1/'+year).day(5).format('L');
     break;
   default:
-    newYears = moment('1/1/'+currentYear).format('L');
+    newYears = moment('1/1/'+year).format('L');
 }
 
 // Martin Luther King, Jr. Day - Third Monday in January
-var martinLutherKingJrDay = moment('1/1/'+currentYear).day(1);
+var martinLutherKingJrDay = moment('1/1/'+year).day(1);
 switch (martinLutherKingJrDay.month()) {
   case 11:
-    martinLutherKingJrDay = moment('1/1/'+currentYear).day(22).format('L');
+    martinLutherKingJrDay = moment('1/1/'+year).day(22).format('L');
     break;
   default:
-    martinLutherKingJrDay = moment('1/1/'+currentYear).day(15).format('L');
+    martinLutherKingJrDay = moment('1/1/'+year).day(15).format('L');
 }
 
 
 // George Washington’s Birthday - Third Monday in February
-var washingtonBirthday = moment('2/1/'+currentYear).day(1);
+var washingtonBirthday = moment('2/1/'+year).day(1);
 switch (washingtonBirthday.month()) {
   case 0:
-    washingtonBirthday = moment('2/1/'+currentYear).day(22).format('L');
+    washingtonBirthday = moment('2/1/'+year).day(22).format('L');
     break;
   default:
-    washingtonBirthday = moment('2/1/'+currentYear).day(15).format('L');
+    washingtonBirthday = moment('2/1/'+year).day(15).format('L');
 }
 
 // Memorial Day - Last Monday in May
-var memorialDay = moment('5/31/'+currentYear).day(1);
+var memorialDay = moment('5/31/'+year).day(1);
 switch (memorialDay.month()) {
   case 5: //June
-    memorialDay = moment('5/31/'+currentYear).day(-6).format('L');
+    memorialDay = moment('5/31/'+year).day(-6).format('L');
     break;
   default:
-    memorialDay = moment('5/31/'+currentYear).day(1).format('L');
+    memorialDay = moment('5/31/'+year).day(1).format('L');
 }
 
-
 // Independence Day - July 4
-var indepenceDay = moment('7/4/'+currentYear).day();
+var indepenceDay = moment('7/4/'+year).day();
 switch (indepenceDay) {
   case 0: //Sunday
-    indepenceDay = moment('7/4/'+currentYear).day(1).format('L');
+    indepenceDay = moment('7/4/'+year).day(1).format('L');
     break;
   case 6: //Saturday
-    indepenceDay = moment('7/4/'+currentYear).day(5).format('L');
+    indepenceDay = moment('7/4/'+year).day(5).format('L');
     break;
   default:
-    indepenceDay = moment('7/4/'+currentYear).format('L');
+    indepenceDay = moment('7/4/'+year).format('L');
 }
 
 // Labor Day - First Monday in September
-var laborDay = moment('9/1/'+currentYear).day(1);
+var laborDay = moment('9/1/'+year).day(1);
 switch (laborDay.month()) {
   case 7: //August
-    laborDay = moment('9/1/'+currentYear).day(8).format('L');
+    laborDay = moment('9/1/'+year).day(8).format('L');
     break;
   default:
-    laborDay = moment('9/1/'+currentYear).day(1).format('L');
+    laborDay = moment('9/1/'+year).day(1).format('L');
 }
 
 // Columbus Day - Second Monday in October
-var columbusDay = moment('10/1/'+currentYear).day(1);
+var columbusDay = moment('10/1/'+year).day(1);
 switch (columbusDay.month()) {
   case 8: //September
-    columbusDay = moment('10/1/'+currentYear).day(15).format('L');
+    columbusDay = moment('10/1/'+year).day(15).format('L');
     break;
   default:
-    columbusDay = moment('10/1/'+currentYear).day(8).format('L');
+    columbusDay = moment('10/1/'+year).day(8).format('L');
 }
 
 // Veterans Day - November 11
-var veteransDay = moment('11/11/'+currentYear).day();
+var veteransDay = moment('11/11/'+year).day();
 switch (veteransDay) {
   case 0: //Sunday
-    veteransDay = moment('11/11/'+currentYear).day(1).format('L');
+    veteransDay = moment('11/11/'+year).day(1).format('L');
     break;
   case 6: //Saturday
-    veteransDay = moment('11/11/'+currentYear).day(5).format('L');
+    veteransDay = moment('11/11/'+year).day(5).format('L');
     break;
   default:
-    veteransDay = moment('11/11/'+currentYear).format('L');
+    veteransDay = moment('11/11/'+year).format('L');
 }
 
 // Thanksgiving Day - 4th Thursday in November
-var thanksgivingDay = moment('11/1/'+currentYear).day(4);
+var thanksgivingDay = moment('11/1/'+year).day(4);
 switch (thanksgivingDay.month()) {
   case 9: //October
-    thanksgivingDay = moment('11/1/'+currentYear).day(32).format('L');
+    thanksgivingDay = moment('11/1/'+year).day(32).format('L');
     break;
   default:
-    thanksgivingDay = moment('11/1/'+currentYear).day(25).format('L');
+    thanksgivingDay = moment('11/1/'+year).day(25).format('L');
 }
 // Christmas Day - December 25
-var christmasDay = moment('12/25/'+currentYear).day();
+var christmasDay = moment('12/25/'+year).day();
 switch (christmasDay) {
   case 0: //Sunday
-    christmasDay = moment('12/25/'+currentYear).day(1).format('L');
+    christmasDay = moment('12/25/'+year).day(1).format('L');
     break;
   case 6: //Saturday
-    christmasDay = moment('12/25/'+currentYear).day(5).format('L');
+    christmasDay = moment('12/25/'+year).day(5).format('L');
     break;
   default:
-    christmasDay = moment('12/25/'+currentYear).format('L');
+    christmasDay = moment('12/25/'+year).format('L');
+}
+
+// Take moment date format L, e.g. "2012"
+function isNationalHoliday(date){
+  var holidays = [
+      newYears,
+      martinLutherKingJrDay,
+      washingtonBirthday,
+      memorialDay,
+      indepenceDay,
+      laborDay,
+      columbusDay,
+      veteransDay,
+      thanksgivingDay,
+      christmasDay
+    ];
+
+  return holidays.includes(date);
 }
